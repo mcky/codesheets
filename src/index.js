3c2b085c0a93c2aef377e7a7fbfa00b2244a11f1
Ross Mackay <ross@mcky.me>
2017-07-26 19:28:04 +0100

    feature: Add promise returning cells and more debug information

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import R from 'ramda'
 import toposort from 'toposort'
 
+const delay = ms =>
+	new Promise(resolve => {
+		setTimeout(resolve, ms)
+	})
+
 const computable = (dependencies, formula) => ({
 	computable: true,
 	dependencies,
@@ -14,11 +19,30 @@ const constant = val => ({
 
 const sheet = {
 	A1: constant(50),
+	A4: constant(30),
 	B1: constant(7),
-	C1: computable(['E1', 'B1'], (E1, B1) => `E1: ${E1}, B1: ${B1}!`),
+	C1: computable(['E1', 'B1'], (E1, B1) => {
+		console.log('computing C1 (From E1, B1)', E1, B1)
+		return `E1: ${E1}, B1: ${B1}!`
+	}),
 	D1: constant(3),
-	E1: computable(['A1'], A1 => A1 + 5),
+	E1: computable(['A1'], A1 => {
+		console.log('computing E1 (from A1)', A1)
+		return A1 + 5
+	}),
 	F1: constant(12),
+	H1: computable(['B1'], B1 => {
+		console.log('computing H1 (from B1) ..slowly', B1)
+		return delay(4000).then(() => B1 * 10)
+	}),
+	G7: computable(['A1', 'A4'], (A1, A4) => {
+		console.log('computing C1 (From A1, A4)', A1, A4)
+		return A1 * A4
+	}),
+	H2: computable(['H1'], H1 => {
+		console.log('computing H2 (from H2)', H1)
+		return `Waiting for H1.. ${H1 * 2}`
+	}),
 }
 
 const getDependencyOrder = R.pipe(
