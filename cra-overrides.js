951a80d97b59f0f1bdb58f5cc75cba7b220a9b61
Ross Mackay <ross@mcky.me>
2017-10-05 23:22:08 +0100

    feature: Add linting & prettier

--- cra-overrides.js
+++ cra-overrides.js
@@ -1,4 +1,16 @@
+const regexesMatch = (regexA, regexB) => String(regexA) === String(regexB)
 
 module.exports = (config, isDev) => {
+
+	const jsConfig = config.module.rules.find(rule =>
+		regexesMatch(rule.test, /\.(js|jsx)$/)
+	)
+
+	if (isDev) {
+		jsConfig.use = []
+	} else {
+		jsConfig.use[0].options.baseConfig.extends = [require.resolve('./.eslintrc')]
+	}
+
 	return config
 }
