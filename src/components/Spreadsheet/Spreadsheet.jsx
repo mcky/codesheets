55d9498cf2a7c569470cea69a536d215c3a8aa75
Ross Mackay <ross@mcky.me>
2017-10-11 12:08:06 +0100

    feature: Add row and column headers

--- src/components/Spreadsheet/Spreadsheet.jsx
+++ src/components/Spreadsheet/Spreadsheet.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { map, merge, evolve } from 'ramda'
+import { pipe, map, merge, evolve, prepend, __ } from 'ramda'
 import { mapProps } from 'recompose'
 
 import ReactDataSheet from 'react-datasheet'
 import 'react-datasheet/lib/react-datasheet.css'
 
+import { mapIndexed, alphabet } from '../../utils'
+
 import './Spreadsheet.css'
+
+const cellDefaults = {
+	width: 35,
+	className: 'SpreadsheetCell',
+}
+const addCellDefaults = map(map(merge(cellDefaults)))
+
+const headerCell = value => ({ value, className: 'HeaderCell' })
+
+const addColHeader = rows =>
+	pipe(mapIndexed((_, i) => headerCell(alphabet[i])), prepend(__, rows))(rows)
+
+const addRowHeader = mapIndexed((row, i) => prepend(headerCell(i), row))
+
+const addHeaders = pipe(addColHeader, addRowHeader)
 
 const Spreadsheet = ({ values }) => (
 	<ReactDataSheet
@@ -21,14 +38,8 @@ Spreadsheet.propTypes = {
 	values: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
-const cellDefaults = {
-	width: 35,
-	className: 'SpreadsheetCell',
-}
-const addCellDefaults = map(map(merge(cellDefaults)))
-
 export default mapProps(
 	evolve({
-		values: addCellDefaults,
+		values: pipe(addCellDefaults, addHeaders),
 	}),
 )(Spreadsheet)
